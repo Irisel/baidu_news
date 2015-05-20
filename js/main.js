d73dad7d247b8878670134ed87f0a7809d49ad14
Irisel <625043769@qq.com>
2015-05-21 02:42:16 +0800

    second alpha

--- js/main.js
+++ js/main.js
@@ -11,18 +11,17 @@ $(function(){
         system.win = p.indexOf("Win") == 0;
         system.mac = p.indexOf("Mac") == 0;
         system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-        if(!(system.win||system.mac||system.xll)){
-            $('.main1').height($('.main0').height());
-            var main3 = $(".main3");
-            $('.m3-bg4').css({'top': (main3.offset().top - 52) + 'px'});
-            $('.m3-bg5').css({'top': (main3.offset().top + main3.height()) -210 + 'px'});
+//        if(!(system.win||system.mac||system.xll)){
+            require(['lib/zepto'], function(zepto){
+
+            })
             require(['phone'], function (phone){
                 phone.phone_init();
             });
-        }else{
-            require(['pc'], function (pc){
-                pc.pc_init();
-            })
-        }
+//        }else{
+//            require(['pc'], function (pc){
+//                pc.pc_init();
+//            })
+//        }
 
 });
